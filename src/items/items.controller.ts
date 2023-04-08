e0431c65d6796e3881355b6fdd29a053d463d660
maeta187 <nexus18789@gmail.com>
2023-04-08 18:37:38 +0900

    ✨ : items/controllerの作成

---
+++ src/items/items.controller.ts
@@ -0,0 +1,29 @@
+import { Controller, Get, Post, Body } from '@nestjs/common'
+import { Item } from './item.model'
+import { ItemsService } from './items.service'
+
+@Controller('items')
+export class ItemsController {
+  constructor(private readonly itemsService: ItemsService) {}
+  @Get()
+  findAll() {
+    return this.itemsService.findAll()
+  }
+
+  @Post()
+  create(
+    @Body('id') id: string,
+    @Body('name') name: string,
+    @Body('price') price: number,
+    @Body('description') description: string
+  ): Item {
+    const item: Item = {
+      id,
+      name,
+      price,
+      description,
+      status: 'ON_SALE'
+    }
+    return this.itemsService.create(item)
+  }
+}
