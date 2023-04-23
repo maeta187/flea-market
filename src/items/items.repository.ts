78eb43aadad4e9eab3d096f2aeeed38aa56007a6
maeta187 <nexus18789@gmail.com>
2023-04-23 17:15:36 +0900

    ✨ : Repository/createItemの実装

---
+++ src/items/items.repository.ts
@@ -0,0 +1,22 @@
+import { Item } from 'src/entities/item.entity'
+import { EntityRepository, Repository } from 'typeorm'
+import { CreateItemDto } from './dto/create-item.dto'
+
+@EntityRepository(Item)
+export class ItemRepository extends Repository<Item> {
+  async createItem(CreateItemDto: CreateItemDto): Promise<Item> {
+    const { name, price, description } = CreateItemDto
+    const item = this.create({
+      name,
+      price,
+      description,
+      status: 'ON_SALE',
+      createdAt: new Date().toISOString(),
+      updatedAt: new Date().toISOString()
+    })
+
+    await this.save(item)
+
+    return item
+  }
+}
