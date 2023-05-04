6d688de3f5016963c7667e0e11cac2f00b2eb80a
maeta187 <nexus18789@gmail.com>
2023-05-04 15:54:58 +0900

    ✨ : CreateUserDtoの作成

---
+++ src/auth/dto/create-user.dto.ts
@@ -0,0 +1,15 @@
+import { IsNotEmpty, IsString, MaxLength, MinLength } from 'class-validator'
+
+export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
+  username: string
+
+  @IsString()
+  @MinLength(8)
+  @MaxLength(32)
+  password: string
+
+  @IsString()
+  status: 'FREE' | 'PREMIUM'
+}
