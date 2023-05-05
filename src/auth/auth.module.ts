2d6b4b42eba812580010d18e321b64a01bd847c1
maeta187 <nexus18789@gmail.com>
2023-05-05 13:29:03 +0900

    ✨ : 認証機能の実装

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,12 +1,24 @@
 import { Module } from '@nestjs/common'
+import { JwtModule } from '@nestjs/jwt'
+import { PassportModule } from '@nestjs/passport'
 import { AuthController } from './auth.controller'
 import { AuthService } from './auth.service'
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { UserRepository } from './user.repository'
+import { JwtStrategy } from './jwt.strategy'
+import { JwtAuthGuard } from './guards/jwt-auth.guard'
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserRepository])],
+  imports: [
+    TypeOrmModule.forFeature([UserRepository]),
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.register({
+      secret: 'secretKey123',
+      signOptions: { expiresIn: 3600 }
+    })
+  ],
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [AuthService, JwtStrategy, JwtAuthGuard],
+  exports: [JwtStrategy, JwtAuthGuard]
 })
 export class AuthModule {}
